eb1ac8866f340fdad9f5eae7ac7974d6f8ba1ad3
edreesz <edriselias@hotmail.com>
2025-02-13 19:22:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 class Student:
-    __attendance = 0
 
-    def __init__(self, name, student_id, gpa):
+
+    def __init__(self, name, student_id, gpa,attendance  ):
         self.name = name
         self.student_id = student_id
         self.gpa = gpa
+        self.attendance= 0
+
 
     def display(self):
         print(self.name, self.student_id, self.gpa)
 
-    @classmethod
-    def get_attendance(cls):
-        return cls.__attendance
 
-    @classmethod
-    def mark_attendance(cls):
-        cls.__attendance += 1  
+    def mark_attendance(self):
+        self.attendance += 1
+    def display_attendance(self):
+        print("Attendance: ", self.attendance)
+
 
 class Course:
     def __init__(self, course_name):
@@ -24,7 +25,7 @@ class Course:
         self.student_count = 0
 
     def add_student(self, student):
-        if isinstance(student, Student):  
+        if isinstance(student, Student):
             self.students.append(student)
             self.student_count += 1
 
@@ -40,13 +41,16 @@ class Course:
             print("Name: "+ student.name + " \n")
 
 
-student1 = Student("alice", "512345", 3.8)
-student2 = Student("bob", "567890", 3.5)
+student1 = Student("alice", "512345", 3.8,0)
+student2 = Student("bob", "567890", 3.5,0)
 
 course = Course("computer science")
 course.add_student(student1)
 course.add_student(student2)
 
 course.list_students()
+student1.mark_attendance()
+print("Attendance for Alice: \n")
+student1.display_attendance()
 student1.mark_attendance()
 print("Attendance for Alice:", student1.get_attendance())
